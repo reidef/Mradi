46dc51cdbfea37a57cb2563610b75b29fb3ce733
Reid Gillette <reid.gillette@gmail.com>
2011-09-08 20:33:16 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mradi::Application.config.secret_token = '6468d6cb5dbb99647c8374a62b13c689fa5b189f5811972c891f28ca554d3e607bfe0495dd165d6fbdee4be8d1d8b7849ec3c6e7eb7b821937d4721f4a5fd107'
